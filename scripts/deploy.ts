ef0487d68ad873ac0325355a00f175b83ea9bef4
EmanCanCode <edouge4@gmail.com>
2025-02-13 23:05:45 -0700

    added test directory and files. added deploy.ts

---
+++ scripts/deploy.ts
@@ -0,0 +1,3 @@
+// will deploy and seed all deployed contracts
+// have a delimiter for each contract to know if we are deploying or not
+// e.g ({ realEstate: true/false, finance: true/false, supplyChain: true/false, ... })
